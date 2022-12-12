eca62fe11669144c5879a0c68d86dd20bf3bfe5d
Kryko7 <61829993+Kryko7@users.noreply.github.com>
2022-12-12 21:02:50 +0530

    Updated a little bit further

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -26,7 +26,7 @@ public class PersistentExpenseManager extends ExpenseManager{
         setAccountsDAO(persistentAccountDAO);
 
 
-
+        // These data will be present at the beginning of the app.
         Account dummyAcct1 = new Account("12345A", "Yoda Bank", "Anakin Skywalker", 10000.0);
         Account dummyAcct2 = new Account("78945Z", "Clone BC", "Obi-Wan Kenobi", 80000.0);
         getAccountsDAO().addAccount(dummyAcct1);
